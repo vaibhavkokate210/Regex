9172cf695fc691efc6d46f48ce600d213201685e
vaibhavkokate210 <vaibhavkokate210@gmail.com>
2021-08-30 13:11:57 +0530

    [Vaibhav] added valid last name is checked

--- src/Regex.java
+++ src/Regex.java
@@ -9,16 +9,29 @@ public class Regex
 		Pattern p=Pattern.compile(reg);
 		return p.matcher(str).matches();
 	}
+	public static boolean getLastName(String str)
+	{
+		String reg="^[A-Z][a-z]{2,}";
+		Pattern p=Pattern.compile(reg);
+		return p.matcher(str).matches();
+	}
 	public static void main(String[] args) 
 	{
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter first Name : ");
-		String str=sc.nextLine();
-		boolean result=getFirstName(str);
-		if(result==true)
+		String firstName=sc.nextLine();
+		System.out.println("Enter Last Name : ");
+		String lastName=sc.nextLine();
+		boolean firstNameResult=getFirstName(firstName);
+		boolean lastNameResult=getFirstName(firstName);
+		if(firstNameResult==true)
 			System.out.println("First Name is correct ");
 		else
 			System.out.println("First Name is not correct");
+		if(lastNameResult==true)
+			System.out.println("Last Name is correct ");
+		else
+			System.out.println("Last Name is not correct");
 	}
 	
 }
